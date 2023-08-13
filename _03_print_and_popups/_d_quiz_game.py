b7af656f8e5ca2ab2850ac9eec887e1964917701
ashwinsingh <ashwinjsingh012@gmail.com>
2023-08-13 13:01:02 -0700

    finished

--- _03_print_and_popups/_d_quiz_game.py
+++ _03_print_and_popups/_d_quiz_game.py
@@ -1,21 +1,33 @@
 from tkinter import messagebox, simpledialog, Tk
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
-    # Make a new window variable, window = Tk()
-    
-    # Hide the window using the window's .withdraw() method
-    
-    # 1. Create a variable to hold the user's score. Set it equal to zero. 
+if __name__ == '__main__':
 
+    # Make a new window variable, window = Tk()
+    window = Tk()
+    # Hide the window using the window's .withdraw() method
+    window.withdraw()
+    # 1. Create a variable to hold the user's score. Set it equal to zero. 
+    score = 0
     # ASK A QUESTION AND CHECK THE ANSWER
 
     #      // 2.  Ask the user a question 
-    
+    question = simpledialog.askstring(title="sup",prompt='can turtles draw?')
     #      // 3.  Use an if statement to check if their answer is correct
-
+    if question == "yes":
+        score=score+1
+        messagebox.showinfo(title='sup',message="well duh")
+    else:
+        score=0
+        messagebox.showinfo(title="i",message="COME ON, OF COURSE THEY CAN")
     #      // 4.  if the user's answer was correct, add one to their score 
- 
+    question = simpledialog.askstring(title='sup',prompt='can pigs fly?')
+    if question == "no":
+        score=score+1
+        messagebox.showinfo(title="hi",message='THANK YOU FOR GETTING THIS ONE RIGHT, THANK YOU!')
+    else:
+        score=score
+        messagebox.showinfo(title="hi",message="HOW ON EARTH DID YOU GET THAT ANSWER LIKE IT'S A METAPHOR FOR SOME THING THAT WILL NEVER HAPPEN ")
     # MAKE MORE QUESTIONS. Ask more questions by repeating the above 
     #      // Option: Subtract a point from their score for a wrong answer
  
